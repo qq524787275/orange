bcc81adb8a5d1ec10c80e7142b9cd53c8571b631
524787275@qq.com <qaioasd520>
2019-07-08 00:19:01 +0800

    update

--- orange_flutter/lib/main.dart
+++ orange_flutter/lib/main.dart
@@ -1,9 +1,35 @@
 import 'dart:ui';
+import 'package:flare_flutter/flare_cache.dart';
+import 'package:orange_flutter/style.dart';
+
 import 'channel.dart';
 import 'package:flutter/material.dart';
 import 'package:flare_flutter/flare_actor.dart';
 
-void main() => runApp(_widgetForRoute(window.defaultRouteName));
+void main() {
+  FlareCache.doesPrune = false;
+  runApp(MainApp());
+}
+
+class MainApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter 学习',
+      theme: ThemeData(
+          brightness: Brightness.light,
+          primarySwatch: Colors.orange,
+          canvasColor: Colors.transparent),
+      home: _widgetForRoute(window.defaultRouteName),
+      navigatorObservers: [GLObserver()],
+    );
+  }
+}
 
 class GLObserver extends NavigatorObserver {
   @override
@@ -20,14 +46,9 @@ class GLObserver extends NavigatorObserver {
 }
 
 Widget _widgetForRoute(String route) {
-  Log.i("hahah", route);
   switch (route) {
     case 'flutter':
-      return MaterialApp(
-        title: 'Flutter学习', // used by the OS task switcher
-        home: new DayListScaffold(),
-        navigatorObservers: [GLObserver()],
-      );
+      return new DayListScaffold();
     default:
       return Center(
         child: Text('路由错误: $route', textDirection: TextDirection.ltr),
@@ -41,22 +62,34 @@ class DayStyle {
   final String flare;
   final String animation;
 
-  DayStyle({this.title, this.desc, this.flare,this.animation});
+  DayStyle({this.title, this.desc, this.flare, this.animation});
 
   static final _all = [
-    DayStyle(title: "day01", desc: "一个时钟动画", flare: "assets/flare/Filip .flr",animation:"idle")
+    DayStyle(
+        title: "demo",
+        desc: "一个动画demo",
+        flare: "assets/flare/Filip.flr",
+        animation: "idle"),
+    DayStyle(
+        title: "Day01",
+        desc: "一个时钟动画,丑是丑了点",
+        flare: "assets/flare/day01.flr",
+        animation: "start")
   ];
 }
 
 class DayListScaffold extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return new Material(
-        child: new Container(
+    return new Scaffold(
+        body: new Container(
+      alignment: Alignment.center,
+      color: contentColor,
       padding: EdgeInsets.fromLTRB(0, 25, 0, 0),
-      child: ListView.builder(
+      child: ListView.separated(
           padding: const EdgeInsets.all(8.0),
           itemCount: DayStyle._all.length,
+          separatorBuilder: (context, index) => const Divider(),
           itemBuilder: (BuildContext context, int index) {
             return Container(
                 height: 100,
@@ -67,8 +100,10 @@ class DayListScaffold extends StatelessWidget {
                     onTap: () => {
                       Navigator.push(context, new MaterialPageRoute(
                           builder: (BuildContext context) {
-                        return Material(
-                          child: Center(
+                        return Scaffold(
+                          body: Container(
+                            alignment: Alignment.center,
+                            color: contentColor,
                             child: FlareActor(
                               DayStyle._all[index].flare,
                               alignment: Alignment.center,
