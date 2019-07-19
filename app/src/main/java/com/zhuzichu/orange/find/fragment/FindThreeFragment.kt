ce71c3f3d5246b9e9a2d926168fc6267d33789a2
zhuzichu <524787275@qq.com>
2019-07-19 18:34:34 +0800

    update

--- app/src/main/java/com/zhuzichu/orange/find/fragment/FindThreeFragment.kt
+++ app/src/main/java/com/zhuzichu/orange/find/fragment/FindThreeFragment.kt
@@ -3,10 +3,13 @@ package com.zhuzichu.orange.find.fragment
 import android.os.Bundle
 import com.zhuzichu.mvvm.base.BaseFragment
 import com.zhuzichu.mvvm.databinding.command.BindingCommand
+import com.zhuzichu.mvvm.utils.dip2px
+import com.zhuzichu.mvvm.widget.banner.ScaleLayoutManager
 import com.zhuzichu.orange.BR
 import com.zhuzichu.orange.R
 import com.zhuzichu.orange.databinding.FragmentFindThreeBinding
 import com.zhuzichu.orange.find.viewmodel.FindThreeViewModel
+import kotlinx.android.synthetic.main.fragment_find_three.*
 
 /**
  * Created by Android Studio.
@@ -20,15 +23,23 @@ class FindThreeFragment : BaseFragment<FragmentFindThreeBinding, FindThreeViewMo
 
     override fun bindVariableId(): Int = BR.viewModel
 
+
     override fun initView() {
         _viewModel.showLoading()
 
-        setErrorCommand(BindingCommand( {
+        setErrorCommand(BindingCommand({
             _viewModel.loadSubjectData()
         }))
+
+
+        recycler.layoutManager = ScaleLayoutManager(_activity, dip2px(1f))
+
+
     }
 
     override fun onLazyInitView(savedInstanceState: Bundle?) {
         _viewModel.loadSubjectData()
+
     }
+
 }
